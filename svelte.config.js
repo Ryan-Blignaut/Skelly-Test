e69681d1c6c4ebed54e2379aceb5973d984dcb46
Ryan Blignaut <ryanblignaut902@gmail.com>
2023-10-17 13:48:00 +0200

    test

---
+++ svelte.config.js
@@ -0,0 +1,36 @@
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte'],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [ vitePreprocess()],
+	
+	vitePlugin: {
+		inspector: true,
+	},
+	/* kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+			fallback: 'index.html' // may differ from host to host
+		})
+	} */
+
+	kit: {
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
+	}
+
+};
+export default config;
