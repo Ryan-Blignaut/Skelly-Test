fae303eabd1868fcbb6797f19ff522ccde7d6d4e
St10092012 <St10092012@vcconnect.edu.za>
2023-10-18 12:57:12 +0200

    url wrong 3

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -6,7 +6,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 export const client = createClient({
-	url: process.env.SECRET_TURSO_DATABASE_URL || '',
+	url: process.env.SECRET_TURSO_DATABASE_URL || 'libsql://skelly-db-st10092012.turso.io',
 	authToken: process.env.SECRET_TURSO_AUTH_TOKEN
 });
 
