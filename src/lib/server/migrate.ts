87d70c90f98646f3cb17bf7c7b623ac8ac53645d
St10092012 <St10092012@vcconnect.edu.za>
2023-10-18 15:13:49 +0200

    url wrong 5

--- src/lib/server/migrate.ts
+++ src/lib/server/migrate.ts
@@ -1,3 +1,4 @@
+/*
 import { migrate } from 'drizzle-orm/libsql/migrator';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client/web';
@@ -27,3 +28,4 @@ main().catch((e) => {
 	console.error(e);
 	process.exit(1);
 });
+*/
