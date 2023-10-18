40235aa624078123617ace07803f1c3c50ad3078
St10092012 <St10092012@vcconnect.edu.za>
2023-10-18 15:37:13 +0200

    Add drizz

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@libsql/client/web';
+import { drizzle } from 'drizzle-orm/libsql';
 
-export const db = createClient({
-	url: process.env.SECRET_TURSO_DATABASE_URL || 'libsql://skelly-db-st10092012.turso.io',
-	authToken: process.env.SECRET_TURSO_AUTH_TOKEN
-});
+export const db = drizzle(
+	createClient({
+		url: process.env.SECRET_TURSO_DATABASE_URL || 'libsql://skelly-db-st10092012.turso.io',
+		authToken: process.env.SECRET_TURSO_AUTH_TOKEN
+	})
+);
