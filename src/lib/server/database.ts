2e15ee35a8fa3eee400e77803240469fb5b36799
St10092012 <St10092012@vcconnect.edu.za>
2023-10-18 12:46:09 +0200

    Env var not working on deployment fix?

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,10 +1,12 @@
 import { createClient } from '@libsql/client/web';
 import { drizzle } from 'drizzle-orm/libsql';
-import { SECRET_TURSO_DATABASE_URL, SECRET_TURSO_AUTH_TOKEN } from '$env/static/private';
+// import { SECRET_TURSO_DATABASE_URL, SECRET_TURSO_AUTH_TOKEN } from '$env/static/private';
+// import * as dotenv from 'dotenv';
+import { env } from '$env/dynamic/private';
 
 export const client = createClient({
-	url: SECRET_TURSO_DATABASE_URL,
-	authToken: SECRET_TURSO_AUTH_TOKEN
+	url: env.SECRET_TURSO_DATABASE_URL,
+	authToken: env.SECRET_TURSO_AUTH_TOKEN
 });
 
 export const db = drizzle(client);
