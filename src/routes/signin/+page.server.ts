bd1d1cddc3caf5917021a58d295f6485fd9be79d
St10092012 <St10092012@vcconnect.edu.za>
2023-10-18 15:46:40 +0200

    basic query

--- src/routes/signin/+page.server.ts
+++ src/routes/signin/+page.server.ts
@@ -1,11 +1,16 @@
 import type { PageServerLoad } from './$types';
-import { db } from "$lib/server/database";
+import { db } from '$lib/server/database';
 
 export const load: PageServerLoad = async ({ request, platform }) => {
+	const { rows } = await db.execute(`
+      SELECT count(*) AS num_tables FROM sqlite_master
+      WHERE type ='table' AND name NOT IN ('libsql_wasm_func_table','_litestream_seq','_litestream_lock')
+    `);
 
-	await db.sync();
+	const numTables = rows[0].num_tables;
+
 	console.log('signin/+page.server.ts: load()');
 	console.log('locals:', request);
 	console.log('locals:', platform);
-	return request;
+	return numTables;
 };
