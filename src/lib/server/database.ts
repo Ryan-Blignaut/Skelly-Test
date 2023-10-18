e79d3e5d4b4fcdd7f180b045993f20bb2de63311
St10092012 <St10092012@vcconnect.edu.za>
2023-10-18 12:55:20 +0200

    url wrong 2

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,14 +1,13 @@
 import { createClient } from '@libsql/client/web';
 import { drizzle } from 'drizzle-orm/libsql';
 // import { SECRET_TURSO_DATABASE_URL, SECRET_TURSO_AUTH_TOKEN } from '$env/static/private';
-// import * as dotenv from 'dotenv';
-import { env } from '$env/dynamic/private';
+// import { env } from '$env/dynamic/private';
+import * as dotenv from 'dotenv';
 
-console.log(env.SECRET_TURSO_DATABASE_URL);
-
+dotenv.config();
 export const client = createClient({
-	url: env.SECRET_TURSO_DATABASE_URL,
-	authToken: env.SECRET_TURSO_AUTH_TOKEN
+	url: process.env.SECRET_TURSO_DATABASE_URL || '',
+	authToken: process.env.SECRET_TURSO_AUTH_TOKEN
 });
 
 export const db = drizzle(client);
